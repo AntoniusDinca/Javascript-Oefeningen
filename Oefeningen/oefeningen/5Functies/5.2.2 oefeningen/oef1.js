89b8e3f4796a89fae656830e4f25767d72e47d0b
Antonius Dinca <antoniusdinca@gmail.com>
2024-10-07 14:36:25 +0200

    5.2.2 added

---
+++ Oefeningen/oefeningen/5Functies/5.2.2 oefeningen/oef1.js
@@ -0,0 +1,6 @@
+let berekenTotaal = function(prijs, aantal){
+    let totaalprijs = prijs* aantal;
+    console.log(`De totaalprijs is ${totaalprijs} euro.`);
+}
+
+berekenTotaal(5, 10);
---
+++ Oefeningen/oefeningen/5Functies/5.2.2 oefeningen/oef2.js
@@ -0,0 +1,6 @@
+function berekenLengte (naam1, naam2){
+    let nieuwelengte = naam1.length + naam2.length;
+    console.log(`De totale lengte van de namen is ${nieuwelengte} tekens`);
+}
+
+berekenLengte("jan", "nathan");
